61add171795cfb4d147fda88c04469f08c7d3d2c
Herka  Fitra <herkafitra@MacBook-Pro-Herka.local>
2020-06-03 05:06:32 +0700

    add welcome screen

---
+++ src/components/Welcome.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <div class="welcome container">
+      <div class="card">
+        <div class="card-content center-align">
+          <h1 class="teal-text" >Hello!</h1>
+          <form @submit.prevent="enterChat">
+            <label for="name">name</label>
+            <input v-model="name" type="text" placeholder="enter message">
+            <p v-if="feedback" class="red-text" >{{feedback}}</p>
+            <button class="btn teal">Enter Chat</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data () {
+    return {
+      name: null,
+      feedback: null
+    }
+  },
+  methods: {
+    enterChat () {
+      console.log(this.name)
+      if (this.name) {
+        // doing something
+        this.$router.push({name: 'Chat', params: {name: this.name}})
+      } else {
+        // if not show this
+        this.feedback = 'enter your name please'
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.welcome {
+  max-width: 400px;
+  margin-top:100px;
+}
+</style>
